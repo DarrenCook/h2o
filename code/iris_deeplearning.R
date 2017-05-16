69ce914de69c5da5df9bc2371f00aa2415bd6914
Duncan Garland <duncan.garland@ntlworld.com>
2017-05-16 15:09:41 +0100

    Changed the paths to the datasets for the R scripts. They didn't match the structure of the repo.

--- code/iris_deeplearning.R
+++ code/iris_deeplearning.R
@@ -1,7 +1,7 @@
 library(h2o)
 h2o.init(nthreads = -1)
 
-datasets <- "https://raw.githubusercontent.com/DarrenCook/h2obook/master/datasets/"
+datasets <- "https://raw.githubusercontent.com/DarrenCook/h2o/bk/datasets/"
 data <- h2o.importFile(paste0(datasets,"iris_wheader.csv"))
 y <- "class"
 x <- setdiff(names(data), y)
